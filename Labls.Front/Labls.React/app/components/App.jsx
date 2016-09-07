133127c9fc4ef871f7e9c79d7c316a1cedae9f07
vorou <voropaev.viktor@gmail.com>
2016-09-08 00:53:46 +0500

    reactpartystarted

--- Labls.Front/Labls.React/app/components/App.jsx
+++ Labls.Front/Labls.React/app/components/App.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
+const Line = (props) =>
+<tr>
+    <th scope="row">{props.data.n}</th>
+    <td>{props.data.name}</td>
+    <td>{props.data.price}</td>
+    <td>{props.data.country}</td>
+    <td>{props.data.pcs}</td>
+</tr>
+
+const LineList = (props) =>
+<table className="table">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Товар</th>
+            <th>Цена</th>
+            <th>Производитель</th>
+            <th>Количество</th>
+        </tr>
+    </thead>
+    <tbody>
+        {props.lines.map(l => <Line data={l} />)}
+    </tbody>
+</table>;
 export default class App extends React.Component {
-  render() {
-    return (
-      <div id="content">
-        <h1>Labls</h1>
-      </div>
-    );
-  }
+    constructor() {
+      super();
+      this.state = {lines: [{n: 1, name: "Свитер женский", price: 7900, country: "Италия", pcs: 7}]};
+    }
+    render() {
+        return <div className="container" id="content"><LineList lines={this.state.lines}/></div>;
+    }
 }
--- Labls.Front/Labls.React/brunch-config.js
+++ Labls.Front/Labls.React/brunch-config.js
@@ -1,15 +1,28 @@
 module.exports = {
-  files: {
-    javascripts: {
-      joinTo: {
-        'vendor.js': /^(?!app)/,
-        'app.js': /^app/
-      }
+    files: {
+        javascripts: {
+            joinTo: {
+                'vendor.js': /^(?!app)/,
+                'app.js': /^app/
+            }
+        },
+        stylesheets: {
+            joinTo: 'app.css'
+        }
     },
-    stylesheets: {joinTo: 'app.css'}
-  },
 
-  plugins: {
-    babel: {presets: ['es2015', 'react']}
-  }
+    plugins: {
+        babel: {
+            presets: ['es2015', 'react']
+        }
+    },
+
+    npm: {
+        enabled: true,
+        whitelist: ["jquery"],
+        globals: {
+            $: 'jquery',
+            jQuery: 'jquery'
+        }
+    }
 };
