5034d4fa1c04e1da9a60cec110f01839ec768328
vorou <voropaev.viktor@gmail.com>
2016-10-27 22:41:40 +0500

    everystick

--- Labls.Front/Labls.React/app/components/App.jsx
+++ Labls.Front/Labls.React/app/components/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+"use strict";
+import React from "react";
 
 const Line = (props) =>
-<tr>
-    <th scope="row">{props.data.n}</th>
-    <td>{props.data.name}</td>
-    <td>{props.data.price}</td>
-    <td>{props.data.country}</td>
-    <td>{props.data.pcs}</td>
-</tr>
-
+    <tr>
+        <th scope="row">{props.data.n}</th>
+        <td>{props.data.name}</td>
+        <td>{props.data.price}</td>
+        <td>{props.data.country}</td>
+        <td>{props.data.pcs}</td>
+    </tr>;
 const LineList = (props) =>
-<table className="table">
-    <thead>
+    <table className="table">
+        <thead>
         <tr>
             <th>#</th>
             <th>Товар</th>
@@ -19,17 +19,23 @@ const LineList = (props) =>
             <th>Производитель</th>
             <th>Количество</th>
         </tr>
-    </thead>
-    <tbody>
-        {props.lines.map(l => <Line data={l} />)}
-    </tbody>
-</table>;
-export default class App extends React.Component {
+        </thead>
+        <tbody>
+        {props.lines.map(l => <Line data={l}/>)}
+        </tbody>
+    </table>;
+
+class App extends React.Component {
     constructor() {
-      super();
-      this.state = {lines: [{n: 1, name: "Свитер женский", price: 7900, country: "Италия", pcs: 7}]};
+        super();
+        this.state = { lines: [{ n: 1, name: "Свитер женский", price: 7900, country: "Италия", pcs: 7 }] };
     }
+
     render() {
-        return <div className="container" id="content"><LineList lines={this.state.lines}/></div>;
+        return <div className="container" id="content">
+                   <LineList lines={this.state.lines}/>
+               </div>;
     }
 }
+
+export default App
--- Labls/Program.cs
+++ Labls/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
+using Humanizer;
 
 namespace Labls
 {
@@ -16,12 +19,39 @@ namespace Labls
 
         private static void Main()
         {
-            var input = new[]
-            {
-                new Item {Name = "Свитер женский", Price = "9500", Country = "Италия", Count = 5},
-                new Item {Name = "Кардиган женский", Price = "19500", Country = "Франция", Count = 10},
-                new Item {Name = "Кардиган женский", Price = "19500", Country = "Франция", Count = 30}
-            };
+	        var inputString = @"
+Платье 12500-00.  2 Италия
+Платье 11500-00.  2 Италия
+Туника. 14500-00  2 Италия
+Джемпер  9500-00.  10 Италия
+Джемпер. 10500-00. 10 Италия
+Шапка.    3950-00.  6 Италия
+Туника.  10500-00.  4 Италия
+джемпер.  12500-00.   6 Италия
+жакет.         13500-00.   4 Италия
+джемпер.   11500-00.    6 Италия
+шапка.         4250-00.    6 Италия
+";
+
+	        var inputStrings =
+		        inputString.Replace(".", "")
+			        .Replace("-00", "")
+			        .Split(new[] {Environment.NewLine}, StringSplitOptions.None)
+			        .Where(x => x.IsNotEmpty())
+			        .Select(x => x.Trim().Transform(To.TitleCase));
+
+	        var nfi = (NumberFormatInfo) CultureInfo.InvariantCulture.NumberFormat.Clone();
+	        nfi.NumberGroupSeparator = " ";
+
+			var input = inputStrings
+		        .Select(x => Regex.Match(x, @"(?<name>[А-Яа-я]+)\s+(?<price>\d+)\s+(?<count>\d+)\s+(?<country>[А-Яа-я]+)"))
+		        .Select(m => new Item
+		        {
+			        Name = m.Groups["name"].Value,
+					Price = m.Groups["price"].Value.ToDecimal().ToString("#,0", nfi),
+					Country = m.Groups["country"].Value,
+					Count = m.Groups["count"].Value.ToInt()
+		        });
 
             File.Delete(GetPath("output-labels.docx"));
             File.Copy(GetPath("labels.docx"), GetPath("output-labels.docx"));
@@ -71,4 +101,22 @@ namespace Labls
         public string Country { get; set; }
         public int Count { get; set; }
     }
+
+	public static class StringExtensions
+	{
+		public static int ToInt(this string s)
+		{
+			return int.Parse(s);
+		}
+
+		public static decimal ToDecimal(this string s)
+		{
+			return decimal.Parse(s);
+		}
+
+		public static bool IsNotEmpty(this string s)
+		{
+			return s.Trim() != "";
+		}
+	}
 }
